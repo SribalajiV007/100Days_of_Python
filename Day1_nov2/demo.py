49e775e840229e625df8c6a680a5d0c972166c9b
Sri Balaji V <sribalajihema2003@gmail.com>
2025-11-02 23:59:32 +0530

    first commit

---
+++ Day1_nov2/demo.py
@@ -0,0 +1,9 @@
+# print("Hello World")
+# print("Hello Balaji\nHello Hema")
+# print("Hello "+ input("What is your name?")+"!")
+
+name = input("Enter your name: ")
+print(len(name))
+
+print(len(input("Enter your name: ")))
+
