9eb91ce7052343d7c1a784ee752eb6858f87861c
Sri Balaji V <sribalajihema2003@gmail.com>
2025-11-09 16:57:54 +0530

    day 3

---
+++ Day_3_nov_9/task.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster")
+height = float(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can have the ride")
+else:
+    print("Sorry, your height must be above 120cm")
